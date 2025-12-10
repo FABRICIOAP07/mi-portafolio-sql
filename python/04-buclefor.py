bfb90d4c7208b317ecc32e101075831c0d836542
Gian Carlo Fabricio Ampuero Peña <fabricio7ampuero@gmail.com>
2025-12-09 21:49:08 -0500

    Add files via upload

---
+++ python/04-buclefor.py
@@ -0,0 +1,10 @@
+#Mejor de acceder a una lista de elementos de un listado
+lenguajes=["python", "Ruby", "PHP", "Java"]
+
+for lenguaje in lenguajes:
+    print(lenguaje)
+    
+i=0
+while i <= len (lenguajes):
+    print(lenguajes[i])
+    i=i+1
