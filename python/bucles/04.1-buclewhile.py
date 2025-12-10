0e227fdb1321ab8f9528f16f5563536ef6c5b5f8
Gian Carlo Fabricio Ampuero Peña <fabricio7ampuero@gmail.com>
2025-12-09 22:12:03 -0500

    Add files via upload

---
+++ python/bucles/04.1-buclewhile.py
@@ -0,0 +1,15 @@
+lenguajes=["python", "Ruby", "PHP", "Java"]
+
+i = 1
+while i <= 5:
+    print(i)
+    i= i + 1
+    
+i = 1
+while i <= 5:
+    print(i * "la rata")
+    i= i + 1
+i=0
+while i <= len (lenguajes):
+    print(lenguajes[i])
+    i=i+1
