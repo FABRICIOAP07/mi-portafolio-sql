5e5588d491fe54bba83ee9944fa1caf13644b845
Gian Carlo Fabricio Ampuero Peña <fabricio7ampuero@gmail.com>
2025-12-09 22:05:02 -0500

    Delete python/05.1-listasmetodos.py

--- python/05.1-listasmetodos.py
+++
@@ -1,8 +0,0 @@
-lenguajes=["python", "Ruby", "PHP", "Java"]
-lenguajes.insert(3,"Go")
-lenguajes.insert(0, "C")
-lenguajes.remove("Ruby")
-print("PHP" in lenguajes)
-lenguajes.clear()
-
-print(len(lenguajes))
